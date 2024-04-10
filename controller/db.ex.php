6289af82eac74a9d1f46e9af49e895e2a922e66a
lucas93220 <91134554+lucas93220@users.noreply.github.com>
2024-04-10 11:33:22 +0200

    fichier exemple code connexionn bdd

---
+++ controller/db.ex.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "lostt";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    exit();
+}
+?>
